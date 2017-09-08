d46141be5ba6291f2664717e460620e5ad7e26a7
Lucas Leandro <lucasleandro1204@gmail.com>
2017-09-08 01:43:32 -0300

    latter I will change this, relax

--- backpack.config.js
+++ backpack.config.js
@@ -3,12 +3,18 @@ const path = require('path');
 module.exports = {
   webpack (config, options, webpack) {
     delete config.entry.main;
+
+    if (options.env == 'development') {
+      config.entry.example = [
+        './dev/index.js', // Just to get use of auto reload.
+      ];
+    }
+
     config.entry.check = [
       './src/check.js',
     ];
+
     config.output.path = path.join(__dirname, 'dist');
-    config.output.filename = 'check.js';
-    config.output.sourceMapFilename = 'check.map';
 
     return config;
   }
